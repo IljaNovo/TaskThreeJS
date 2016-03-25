f8cef43b365d3f6b680939839c7b3108700ecbe4
Ilya Novozhilov <ilja.keinn@mail.ru>
2016-03-25 10:45:48 +0300

    add button deleteDate and deleteAllbutton in index.html

---
+++ Insurance.js
@@ -0,0 +1,7 @@
+function Insurance(incurenceType, insurer, saleDate, discount, insurancePremium) {
+    this._incurenceType = incurenceType;
+    this._insurer = insurer;
+    this._saleDate = saleDate;
+    this._discount = discount;
+    this._insurancePremium = insurancePremium;
+}
