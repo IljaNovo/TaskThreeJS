6268631a99ae40e3f539c13b03e4388d9843e2a2
Ilya Novozhilov <ilja.keinn@mail.ru>
2016-03-27 06:48:34 +0300

    implemented show date storage

--- DisplayDateService.js
+++ DisplayDateService.js
@@ -1,11 +1,23 @@
 var displayDateService = (function() {
     
     function showDate() {
-        var displayTag = document.getElementById("right");
+        var displayTag = document.getElementById("storageDate");
         
+        displayTag.innerHTML = "";
         
+        var currentItem;
         
-        displayTag.innerHTML += "<br />Какой хороший день!";
+        for (var i = 0; i < localStorage.length; ++i) {
+            currentItem = dateManager.getDateStorage(localStorage.key(i));
+            
+            displayTag.innerHTML += "<br />" + 
+                "Вид страховки: " + currentItem.type + "<br />" +
+                "Номер полиса: " + currentItem.number + "<br />" +
+                "Страхователь: " + currentItem.insurer + "<br />" +
+                "Дата продажи: " + currentItem.saleDate + "<br />" +
+                "Страховая премия: " + currentItem.premium + "<br />" +
+                "Скидка: " + currentItem.discount + "<br />";
+        }
     }
     return {
         showDate : showDate
